f7a58d307343a09856055941dd6e6174192a38fc
AlonaNihalchuk <acetnikopol@gmail.com>
2021-05-18 01:56:01 +0200

    добавляет нужные файлы в проект

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import './sass/main.scss';
+import menuElementTpl from './menu.hbs';
+import menuElements from './menu.json';
+import './styles.css';
+
+console.log(menuElementTpl(menuElements[1]));
+
+const menuRef = document.querySelector('.js-menu');
+
+const markUpMenu = makeMarkupMenu(menuElements);
+
+menuRef.insertAdjacentHTML('beforeend', markUpMenu);
+
+function makeMarkupMenu(menuElements) {
+  return menuElementTpl(menuElements);
+}
