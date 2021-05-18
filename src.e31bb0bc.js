249b633648f88ae590963766d51577f5136a7baf
AlonaNihalchuk <acetnikopol@gmail.com>
2021-05-18 02:16:50 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -2147,9 +2147,9 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 var templateFunction = _handlebars.default.template({
-  "compiler": [8, ">= 4.3.0"],
-  "main": function main(container, depth0, helpers, partials, data) {
-    var helper,
+  "1": function _(container, depth0, helpers, partials, data) {
+    var stack1,
+        helper,
         alias1 = depth0 != null ? depth0 : container.nullContext || {},
         alias2 = container.hooks.helperMissing,
         alias3 = "function",
@@ -2162,49 +2162,125 @@ var templateFunction = _handlebars.default.template({
       return undefined;
     };
 
-    return "<li class='menu__item'>\r\n  <article class='card'>\r\n    <img\r\n      src='https://s1.eda.ru/StaticContent/Photos/140812180013/140820212258/p_O.jpg'\r\n      alt='" + alias4((helper = (helper = lookupProperty(helpers, "name") || (depth0 != null ? lookupProperty(depth0, "name") : depth0)) != null ? helper : alias2, _typeof(helper) === alias3 ? helper.call(alias1, {
+    return "  <li class='menu__item'>\r\n    <article class='card'>\r\n      <img src='" + alias4((helper = (helper = lookupProperty(helpers, "image") || (depth0 != null ? lookupProperty(depth0, "image") : depth0)) != null ? helper : alias2, _typeof(helper) === alias3 ? helper.call(alias1, {
+      "name": "image",
+      "hash": {},
+      "data": data,
+      "loc": {
+        "start": {
+          "line": 4,
+          "column": 16
+        },
+        "end": {
+          "line": 4,
+          "column": 25
+        }
+      }
+    }) : helper)) + "' alt='" + alias4((helper = (helper = lookupProperty(helpers, "name") || (depth0 != null ? lookupProperty(depth0, "name") : depth0)) != null ? helper : alias2, _typeof(helper) === alias3 ? helper.call(alias1, {
       "name": "name",
       "hash": {},
       "data": data,
       "loc": {
         "start": {
-          "line": 5,
-          "column": 11
+          "line": 4,
+          "column": 32
         },
         "end": {
-          "line": 5,
-          "column": 19
+          "line": 4,
+          "column": 40
         }
       }
-    }) : helper)) + "'\r\n      class='card__image'\r\n    />\r\n    <div class='card__content'>\r\n      <h2 class='card__name'>" + alias4((helper = (helper = lookupProperty(helpers, "name") || (depth0 != null ? lookupProperty(depth0, "name") : depth0)) != null ? helper : alias2, _typeof(helper) === alias3 ? helper.call(alias1, {
+    }) : helper)) + "' class='card__image' />\r\n      <div class='card__content'>\r\n        <h2 class='card__name'>" + alias4((helper = (helper = lookupProperty(helpers, "name") || (depth0 != null ? lookupProperty(depth0, "name") : depth0)) != null ? helper : alias2, _typeof(helper) === alias3 ? helper.call(alias1, {
       "name": "name",
       "hash": {},
       "data": data,
       "loc": {
         "start": {
-          "line": 9,
-          "column": 29
+          "line": 6,
+          "column": 31
         },
         "end": {
-          "line": 9,
-          "column": 37
+          "line": 6,
+          "column": 39
         }
       }
-    }) : helper)) + "</h2>\r\n      <p class='card__price'>\r\n        <i class='material-icons'> monetization_on </i>\r\n        " + alias4((helper = (helper = lookupProperty(helpers, "price") || (depth0 != null ? lookupProperty(depth0, "price") : depth0)) != null ? helper : alias2, _typeof(helper) === alias3 ? helper.call(alias1, {
+    }) : helper)) + "</h2>\r\n        <p class='card__price'>\r\n          <i class='material-icons'> monetization_on </i>\r\n          " + alias4((helper = (helper = lookupProperty(helpers, "price") || (depth0 != null ? lookupProperty(depth0, "price") : depth0)) != null ? helper : alias2, _typeof(helper) === alias3 ? helper.call(alias1, {
       "name": "price",
       "hash": {},
       "data": data,
       "loc": {
         "start": {
-          "line": 12,
-          "column": 8
+          "line": 9,
+          "column": 10
         },
         "end": {
-          "line": 12,
-          "column": 17
+          "line": 9,
+          "column": 19
         }
       }
-    }) : helper)) + "\r\n      </p>\r\n\r\n      <p class='card__descr'>Ароматный, сытный, шипящий домашний картофель, фаршированный\r\n        сметанно-беконной начинкой, — это очень простой и очень эффектный способ накормить большое\r\n        количество человек, практически не потратив на готовку ни сил, ни времени. Обычную картошку\r\n        при желании тут можно заменить на сладкий батат, а в начинку добавить, к примеру, кукурузу\r\n        или сладкий красный перец.</p>\r\n\r\n      <ul class='tag-list'>\r\n        <li class='tag-list__item'>Картофель</li>\r\n        <li class='tag-list__item'>Чеснок</li>\r\n        <li class='tag-list__item'>Сметана</li>\r\n        <li class='tag-list__item'>Бекон</li>\r\n        <li class='tag-list__item'>Твердый сыр</li>\r\n        <li class='tag-list__item'>Зеленый лук</li>\r\n      </ul>\r\n    </div>\r\n\r\n    <button class='card__button button'>\r\n      <i class='material-icons button__icon'> shopping_cart </i>\r\n      В корзину\r\n    </button>\r\n  </article>\r\n</li>";
+    }) : helper)) + "\r\n          кредитов\r\n        </p>\r\n\r\n        <p class='card__descr'>\r\n          " + alias4((helper = (helper = lookupProperty(helpers, "description") || (depth0 != null ? lookupProperty(depth0, "description") : depth0)) != null ? helper : alias2, _typeof(helper) === alias3 ? helper.call(alias1, {
+      "name": "description",
+      "hash": {},
+      "data": data,
+      "loc": {
+        "start": {
+          "line": 14,
+          "column": 10
+        },
+        "end": {
+          "line": 14,
+          "column": 25
+        }
+      }
+    }) : helper)) + "\r\n        </p>\r\n\r\n        <ul class='tag-list'>\r\n" + ((stack1 = lookupProperty(helpers, "each").call(alias1, depth0 != null ? lookupProperty(depth0, "ingredients") : depth0, {
+      "name": "each",
+      "hash": {},
+      "fn": container.program(2, data, 0),
+      "inverse": container.noop,
+      "data": data,
+      "loc": {
+        "start": {
+          "line": 18,
+          "column": 10
+        },
+        "end": {
+          "line": 20,
+          "column": 19
+        }
+      }
+    })) != null ? stack1 : "") + "        </ul>\r\n      </div>\r\n\r\n      <button class='card__button button'>\r\n        <i class='material-icons button__icon'> shopping_cart </i>\r\n        В корзину\r\n      </button>\r\n    </article>\r\n  </li>\r\n";
+  },
+  "2": function _(container, depth0, helpers, partials, data) {
+    return "            <li class='tag-list__item'>" + container.escapeExpression(container.lambda(depth0, depth0)) + "</li>\r\n";
+  },
+  "compiler": [8, ">= 4.3.0"],
+  "main": function main(container, depth0, helpers, partials, data) {
+    var stack1,
+        lookupProperty = container.lookupProperty || function (parent, propertyName) {
+      if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+        return parent[propertyName];
+      }
+
+      return undefined;
+    };
+
+    return (stack1 = lookupProperty(helpers, "each").call(depth0 != null ? depth0 : container.nullContext || {}, depth0, {
+      "name": "each",
+      "hash": {},
+      "fn": container.program(1, data, 0),
+      "inverse": container.noop,
+      "data": data,
+      "loc": {
+        "start": {
+          "line": 1,
+          "column": 0
+        },
+        "end": {
+          "line": 30,
+          "column": 9
+        }
+      }
+    })) != null ? stack1 : "";
   },
   "useData": true
 });
@@ -2388,7 +2464,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58740" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50243" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
